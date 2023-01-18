da428d76f00fbe21baa83bee46d816d1dc1cce91
nsemirdev <nsemirdev@gmail.com>
2023-01-18 16:01:17 +0300

    added color structure

---
+++ ToDoApp/ToDoApp/Colors/IPColor.swift
@@ -0,0 +1,18 @@
+//
+//  IPColor.swift
+//  ToDoApp
+//
+//  Created by Emir Alkal on 18.01.2023.
+//
+
+import UIKit
+
+struct IPColor {
+    static let actionDarkPurple  = UIImage(named: "ActionPrimaryPurple100")!
+    static let actionLightPurple = UIImage(named: "ActionPrimaryPurple50")!
+    static let textPrimary       = UIImage(named: "TextPrimaryBlack")!
+    static let textSecondary     = UIImage(named: "TextSecondaryGray")!
+    static let helperRed         = UIImage(named: "HelperRed")!
+    static let helperGreen       = UIImage(named: "HelperGreen")!
+    static let helperYellow      = UIImage(named: "HelperYellow")!
+}
