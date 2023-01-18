17cccd9565094bd23746c7b95266bf9543e16de3
nsemirdev <nsemirdev@gmail.com>
2023-01-18 15:22:49 +0300

    created workspace and added ToDoApp target

---
+++ ToDoApp/ToDoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoApp
+//
+//  Created by Emir Alkal on 18.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemTeal
+    }
+
+
+}
+
